9273d3e464811e4fa3c01d5f0e68d93c6a9fb58a
Juan Briones <jbrionesgmm@gmail.com>
2023-03-24 15:28:24 -0600

    setting up backend but not working

---
+++ routes/tasks.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const tasksController = require("../controllers/tasks");
+
+
+router.get("/", tasksController.getIndex);
+
+router.get('/tasks', tasksController.getTasks)
+
+router.get('/tasks/:id', tasksController.getTask)
+
+router.post("/tasks", tasksController.createTask);
+
+router.put("/tasks/:id", tasksController.toggleTask);
+
+router.delete("/tasks/:id", tasksController.deleteTask);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+const cors = require('cors');
+const tasksRoutes = require("./routes/tasks");
+const connectDB = require("./config/database");
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+console.log(process.env.DB_STRING)
+
+
+app.use(cors())
+
+//Connect To Database
+connectDB();
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Setup Sessions - stored in MongoDB
+// app.use(
+//     session({
+//       secret: "keyboard cat",
+//       resave: false,
+//       saveUninitialized: false,
+//       store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//     })
+//   );
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", tasksRoutes);
+
+
+  //Server Running
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+  });
